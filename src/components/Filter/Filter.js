13611d16ebac99874a1b03a8703598e4daf2374c
kasiian-37 <65597251+kasiian-37@users.noreply.github.com>
2021-02-24 20:26:59 +0200

    Create Filter.js

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import styles from './Filter.module.css';
+
+function Filter({ initialValue, onFilterChange }) {
+  return (
+    <div className={styles.inputStyle}>
+      <label className={styles.filterLabel}>
+        Find contacts by name:
+        <input
+          className={styles.filterInput}
+          type="text"
+          name="name"
+          value={initialValue}
+          onChange={e => {
+            onFilterChange(e.target.value);
+          }}
+        />
+      </label>
+    </div>
+  );
+}
+
+Filter.defaultProps = {
+  initialValue: '',
+};
+Filter.propTypes = PropTypes.shape({
+  initialValue: PropTypes.string,
+  onFilterChange: PropTypes.func.isRequired,
+}).isRequired;
+export default Filter;
